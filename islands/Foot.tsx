66f634a7e1c63a0a2e7b065fa53c63ab66899176
Yorch <janderaster@protonmail.com>
2024-03-27 16:28:47 -0500

    Basic interface
    Currently evaluating with experts about the scope and general function
    of the application. Pending: member's contact information.

---
+++ islands/Foot.tsx
@@ -0,0 +1,28 @@
+export default function Foot(){
+
+    return (
+        <footer>
+        <div>
+            <p>Este aplicativo es software libre 😎</p>
+            <p>Licencia <a href="">GPLv3</a> </p>
+            <p><a href="https://codeberg.org/luzxyz/librered-dev" target="_blank" >Repositorio principal en Codeberg</a></p>
+            <p><a href="" target="_blank">Repositorio mirror en Sourceforge</a></p>
+            <p><a href="https://github.com/luzxyz/Libreradio-prod" target="_blank">Repositorio mirror en Github</a> (Deployment)</p>
+        </div>
+        <div>
+            <h5>Contacto de los creadores 🔥</h5>
+            <a href="mailto:janderaster@protonmail.com">Jorge Gutierrez</a>
+            <p>janderaster@protonmail.com</p>
+            <a href="mailto:andres@mail.com">Andres Parra Espejo</a>
+            <p>andres@mail.com</p>
+            <a href="mailto:giovanny@mail.com">Giovanny Pira Aldana</a>
+            <p>giovanny@mail.com</p>
+        </div>
+        <div>
+            <h5>Con la colaboración de 🚀</h5>
+            <p>Universidad Distrital FJC - Bogotá</p>
+        </div>
+        </footer>
+    );
+
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,13 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
+import Foot from "../islands/Foot.tsx";
 
 export default function Home() {
-  const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">LibreRadio</h1>
-        <p class="my-4">
-            Funciona!! 
-        </p>
-        <Counter count={count} />
-      </div>
+    <div class="main-page">
+        <div class="section-title">
+            <h1 class="main-title"><span class="star-font">Libre</span>Radio</h1>
+            <p>Hecho en Fresh 🍋</p>
+        </div>
+        <Foot/>
     </div>
   );
 }
